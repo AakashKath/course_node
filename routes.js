086c01f51b8129aa01addd0429dbb515dca57219
Aakash Kath <aakash.kath.258@gmail.com>
2019-07-03 15:05:41 +0530

    basic form and splitting

---
+++ routes.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/'){
+        res.write('<html>');
+        res.write('<head><title>First</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST'){
+        const body = []
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            // fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
+    res.setHeader('content-type', 'text/html');
+    res.write('<html> <head> <title> First </title> </head> <body> <h1> Hi!! </h1> </body> </html>');
+    res.end();
+};
+
+module.exports = requestHandler;
